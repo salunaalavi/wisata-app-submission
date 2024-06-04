7f9443df49da152caedb8e9169b6db1bbd3a114b
Saluna Alavi <salunaalavi@gmail.com>
2024-06-04 13:02:03 +0700

    fix: set value fallback on the server side to be dark

--- plugins/color-scheme.server.ts
+++ plugins/color-scheme.server.ts
@@ -3,18 +3,17 @@ import type { ColorModeInstance } from '@/types/color-scheme';
 /**
  * Will be available in the runtime
  */
-import { value } from '#color-mode-options'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const colorMode = nuxtApp.ssrContext?.islandContext
     ? ref({})
     : useState<ColorModeInstance>('color-mode', () => reactive({
-      value,
+      value: "dark",
       unknown: true,
     })).value
 
   const htmlAttrs: Record<string, string> = {}
-  
+
   useHead({ htmlAttrs })
 
   return {
